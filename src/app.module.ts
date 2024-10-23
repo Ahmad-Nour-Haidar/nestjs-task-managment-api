71835d65538d4e99d5aabd7c3fdbc52f596c92d8
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-24 00:38:27 +0300

    bla bla

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,96 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// const STAGE = process.env.STAGE || 'dev';
+//
+// const path = !STAGE ? '.env' : `.env.${STAGE}`;
+//
+// console.log(path);
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       envFilePath: path,
+//       ignoreEnvFile: true,
+//       isGlobal: true,
+//     }),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       inject: [ConfigService],
+//       useFactory: async (configService: ConfigService) => {
+//         // Create a logger instance
+//         const logger = new Logger('TypeOrmModule');
+//
+//         logger.debug(`STAGE: ${process.env.STAGE}`); // Be careful with passwords!
+//
+//         // Log environment variables (be cautious with sensitive info)
+//         logger.log(`Database Host: ${configService.get('DB_HOST')}`);
+//         logger.log(`Database Port: ${configService.get('DB_PORT')}`);
+//         logger.log(`Database Username: ${configService.get('DB_USERNAME')}`);
+//         logger.log(`Database Name: ${configService.get('DB_DATABASE')}`);
+//         logger.debug(`Database Password: ${configService.get('DB_PASSWORD')}`); // Be careful with passwords!
+//
+//         return {
+//           type: 'postgres',
+//           host: configService.get<string>('DB_HOST'),
+//           port: configService.get<number>('DB_PORT'),
+//           username: configService.get<string>('DB_USERNAME'),
+//           password: configService.get<string>('DB_PASSWORD'),
+//           database: configService.get<string>('DB_DATABASE'),
+//           autoLoadEntities: true,
+//           synchronize: true,
+//         };
+//       },
+//     }),
+//     TasksModule,
+//     AuthModule,
+//   ],
+// })
+// path: ,
+//
+// const logger = new Logger('AppModule');
+// logger.debug(`Current Working Directory: ${process.cwd()}`);
+// const envFilePath = path.resolve(
+//   process.cwd(),
+//   !process.env.STAGE ? '.env' : `.env.stage.${process.env.STAGE}`,
+// );
+// logger.debug(`envFilePath: ${envFilePath}`);
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      // envFilePath: `.env.stage.dev`,
+      isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        // Create a logger instance
+        // const logger = new Logger('TypeOrmModule');
+
+        // logger.debug(`STAGE: ${process.env.STAGE}`);
+
+        // Log environment variables (be cautious with sensitive info)
+        // logger.log(`Database Host: ${configService.get('DB_HOST')}`);
+        // logger.log(`Database Port: ${configService.get('DB_PORT')}`);
+        // logger.log(`Database Username: ${configService.get('DB_USERNAME')}`);
+        // logger.log(`Database Name: ${configService.get('DB_DATABASE')}`);
+        // logger.debug(`Database Password: ${configService.get('DB_PASSWORD')}`); // Be careful with passwords!
+
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
     }),
     TasksModule,
     AuthModule,
